c62a685c67ee1d785fc0dbb23b708b99bdac24fe
Marcoco759212 <marcoco.mahr@gmail.com>
2022-08-24 13:10:33 -0500

    Ejercicio de las sesiones 1, 2 y 3 - ContactComponents

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-// import Greeting from './components/pure/greeting';
-import TaskListComponent from './components/container/task_list';
+import ContactListComponent from './components/container/contact_list';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Greeting name="Marco"></Greeting>   */}
-        <TaskListComponent></TaskListComponent>
+        <ContactListComponent></ContactListComponent>
       </header>
     </div>
   );
---
+++ src/models/contact.class.js
@@ -0,0 +1,14 @@
+export class Contact {
+
+    name = '';
+    lastName = '';
+    email = '';
+    isConected = false;
+
+    constructor (name, lastName, email, isConected){
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.isConected = isConected;
+    }
+}
